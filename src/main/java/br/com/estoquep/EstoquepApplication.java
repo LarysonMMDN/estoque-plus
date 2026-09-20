ff578434f990bb72648f066efea977a532dce621
Laryson Martins <larysonmartins008@gmail.com>
2026-09-20 13:49:29 -0300

    Primeiro commit

---
+++ src/main/java/br/com/estoquep/EstoquepApplication.java
@@ -0,0 +1,13 @@
+package br.com.estoquep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoquepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoquepApplication.class, args);
+	}
+
+}
